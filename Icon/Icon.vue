7b044f31ba2e93ae1533bc9298eccb02dd3ef769
xank <xank@qq.com>
2018-06-07 22:27:00 +0800

    init

---
+++ Icon/Icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="icon--pep" :class="'icon__' + name"></div>
+</template>
+
+<script>
+export default {
+  name: 'att-icon',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() { },
+
+  methods: {}
+}
+
+</script>
+
