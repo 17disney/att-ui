739c459bd66e0526ff78b44820555696424e3843
xank <xank@qq.com>
2018-06-15 22:29:33 +0800

    icon

--- Icon/Icon.vue
+++ Icon/Icon.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="icon--pep" :class="'icon__' + name"></div>
+  <span class="att-icon icon--pep" :class="'icon__' + name"></span>
 </template>
 
 <script>
+/*
++-----------------------------------------------------------------------------------------------------------------------
+| Author: xank <xank@qq.com>  Blog：https://www.xank.cn
++-----------------------------------------------------------------------------------------------------------------------
+| Att-Icon
+|
+*/
+
+const NAME = 'att-icon'
+
 export default {
-  name: 'att-icon',
+  name: NAME,
   props: {
     name: {
       type: String,
       default: ''
     }
-  },
-  data() {
-    return {
-    }
-  },
-
-  components: {},
-
-  computed: {},
-
-  mounted() { },
-
-  methods: {}
+  }
 }
 
 </script>
